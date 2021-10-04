b7d3ccaed13ff29b041627ceaa35659abe4c2a26
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-04 10:21:27 -0400

    Add files via upload

--- Lab 3/mom1.py
+++ Lab 3/mom1.py
@@ -3,7 +3,27 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
+def respond(response):
 
+    if response == 1:
+        print("should be speaking")
+        engine.say("Hi honey you look tired, did you get enough sleep?")
+    if response == 3:
+        engine.say("I am sure that is not true, but if you are worried you can skip soccer tomorrow so you will have more time to study")
+    if response == 4:
+        engine.say("I am sorry, maybe try to avoid her for a little while.  No sense in stooping to her level")
+    if response == 5:
+        engine.say("What would you like for dinner?")
+    if response == 6:
+        engine.say("You always want hamburgers and pizza.  I do not have the stuff for that.  How about some nice chicken and rice with a salad?")
+    if response == 7:
+        engine.say("I am sorry to hear that, tell me all about it while i make us some coffee")
+    if response == 8:
+        engine.say("Hi honey how was school?")
+    if response == 9:
+        engine.say("How was work today?")
+    if response == 2:
+        engine.say("sit up straight")
 
 
 # RATE
@@ -23,30 +43,8 @@ response = int(response)
 
 print(response)
 
-if response == 1:
-    print("should be speaking")
-    engine.say("Hi honey you look tired, did you get enough sleep?")
-if response == 3:
-    engine.say("I am sure that is not true, but if you are worried you can skip soccer tomorrow so you will have more time to study")
-if response == 4:
-    engine.say("I am sorry, maybe try to avoid her for a little while.  No sense in stooping to her level")
-if response == 5:
-    engine.say("What would you like for dinner?")
-if response == 6:
-    engine.say("You always want hamburgers and pizza.  I do not have the stuff for that.  How about some nice chicken and rice with a salad?")
-if response == 7:
-    engine.say("I am sorry to hear that, tell me all about it while i make us some coffee")
-if response == 8:
-    engine.say("Hi honey how was school?")
-if response == 9:
-    engine.say("How was work today?")
-if response == 2:
-    engine.say("sit up straight")
-   
-  
+respond(response)
 
-
-#engine.say("Hello say lur")
 engine.runAndWait()
 
 
