11ed81258f32cb051d9bf4a0958fb4b6f33495b8
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-05 07:03:54 -0400

    Update miccer.sh

--- Lab 3/miccer.sh
+++ Lab 3/miccer.sh
@@ -3,3 +3,4 @@
 espeak -ven+f2 -k5 -s150 --stdout "May I have your phone number?" | aplay
 
 arecord -D hw:3,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+python3 test_words.py recorded_mono.wav
