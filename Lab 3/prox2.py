66756b8f5bbcfef73c2ff14cf555980c9d4d4e5a
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-07 18:22:14 -0400

    Add files via upload

--- Lab 3/prox2.py
+++ Lab 3/prox2.py
@@ -7,7 +7,7 @@ int_pin = digitalio.DigitalInOut(board.D5)
 apds = APDS9960(i2c, interrupt_pin=int_pin)
 
 apds.enable_proximity = True
-apds.proximity_interrupt_threshold = (0, 500)
+apds.proximity_interrupt_threshold = (0, 256)
 apds.enable_proximity_interrupt = True
 
 apds.enable_color = True
