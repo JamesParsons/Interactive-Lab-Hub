d93585ab1d91ab7396edf29ccddc3ce7e42988ac
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-08 10:17:26 -0400

    Add files via upload

--- Lab 3/rec4.py
+++ Lab 3/rec4.py
@@ -1,6 +1,11 @@
 import speech_recognition as sr
 
 
+r = sr.Recognizer()
+speech = sr.Microphone(device_index=2)
+stopper = 0
+
+
 def findResponse(spoken):
     if "hello" in spoken:
         print("found hello")
@@ -8,29 +13,28 @@ def findResponse(spoken):
     if "mom" in spoken:
         print("Hi Dear")
     if "Mom" in spoken:
-        print("Hi Dear")    
+        print("Hi Dear")   
+    if "goodbye" in spoken:
+        stopper = 1
 
+def getSpeech():
 
-
-
-
-
-
-
-r = sr.Recognizer()
-speech = sr.Microphone(device_index=2)
-with speech as source:
-    print("say something!…")
-    #audio = r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
-try:
-    recog = r.recognize_google(audio, language = 'en-US')
-
-    print("You said: " + recog)
-    findResponse(recog)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    with speech as source:
+        print("say something!…")
+        #audio = r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+    try:
+        recog = r.recognize_google(audio, language = 'en-US')
+    
+        print("You said: " + recog)
+        findResponse(recog)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    
+while stoper == 0:
+    getSpeech()
+    
     
     
