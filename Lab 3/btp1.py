54d0bd521179a372affb9a71fd26ec31e9260071
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-09 10:27:17 -0400

    Add files via upload

--- Lab 3/btp1.py
+++ Lab 3/btp1.py
@@ -1,8 +1,12 @@
 import subprocess
+import time
 
 #subprocess.call("ls")
 
 subprocess.call("arecord -D hw:3,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav")
+
+sleep(7)
+
 subprocess.call("python3 test_words2.py recorded_mono.wav")
 
 
