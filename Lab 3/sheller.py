b509423933da15026326c4d5ee7ec4ea22376ba3
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-09 17:40:50 -0400

    Add files via upload

--- Lab 3/sheller.py
+++ Lab 3/sheller.py
@@ -10,16 +10,19 @@ import subprocess
 #arecord = subprocess.run(['arecord', '-D', 'hw:3,0', '-f', 'cd', '-c1', '-r', '48000', '-d', '5', '-t', 'wav', 'recorded_mono.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
-subprocess.run(['arecord', '-D', 'hw:3,0', '-f', 'cd', '-c1', '-r', '48000', '-d', '5', '-t', 'wav', 'recorded_mono.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
+#subprocess.run(['arecord', '-D', 'hw:3,0', '-f', 'cd', '-c1', '-r', '48000', '-d', '5', '-t', 'wav', 'recorded_mono.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+##subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
 
 
-result = subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
+#result = subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
 
-print(result)
+#print(result)
 
-if "dinner" in result:
-    print("found dinner")
+#if "dinner" in result:
+    #print("found dinner")
+    
+    
+result = subprocess.run("arecord -D hw:3,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav | python3 test_words2.py recorded_mono.wav", shell=True)
 
 
 # converting the shell to python gotten from:
