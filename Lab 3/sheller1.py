8edc30485513c373c980a32edb5261d344161be9
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-10 10:39:24 -0400

    Add files via upload

--- Lab 3/sheller1.py
+++ Lab 3/sheller1.py
@@ -7,10 +7,12 @@ import subprocess
 subprocess.run(['arecord', '-D', 'hw:3,0', '-f', 'cd', '-c1', '-r', '48000', '-d', '5', '-t', 'wav', 'recorded_mono.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 ##subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
 
-result = subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
+#result = subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
+
+result = subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'], stdout=PIPE)
 
 print("result =", result)
-print("stdout= ", type(result.stdout))
+print("stdout= ", type(result.stdout))n
 
 #print(result["text"])
 
