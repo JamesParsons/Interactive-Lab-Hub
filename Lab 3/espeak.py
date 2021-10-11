612ff414ed2b988882cbe3664e18ed1c2a6a9131
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-11 19:07:07 -0400

    Add files via upload

--- Lab 3/espeak.py
+++ Lab 3/espeak.py
@@ -1,4 +1,4 @@
 from subprocess import Popen, PIPE, STDOUT
 
 text = u"René Descartes"
-p = Popen(['espeak', '-b', '1'], stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+p = Popen(['espeak', '-b', '1'], stdin=PIPE, stderr=STDOUT)
