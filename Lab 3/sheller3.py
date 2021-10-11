c9b41cc5874a98861b43d27aa5842b3dffd4ea7e
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-11 16:51:36 -0400

    Add files via upload

--- Lab 3/sheller3.py
+++ Lab 3/sheller3.py
@@ -4,7 +4,7 @@ import time
 
 
 
-while true:
+while True:
     subprocess.run(['arecord', '-D', 'hw:3,0', '-f', 'cd', '-c1', '-r', '48000', '-d', '5', '-t', 'wav', 'recorded_mono.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     result = subprocess.run(['python3', 'test_words2.py', 'recorded_mono.wav'])
     sleep(10)
