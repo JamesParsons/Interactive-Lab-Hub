18f271bc097b59ed16e16a8e6d6346e9dc231895
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-10 21:08:44 -0400

    Add files via upload

--- Lab 3/test_words2.py
+++ Lab 3/test_words2.py
@@ -39,7 +39,7 @@ while True:
 print("from test_words2 ", rec.FinalResult())
 #print("type = ", type(rec.FinalResult))
 print("resulter ", resulter)
-print("dict try ", resulter["result"])
+print("dict try ", resulter["result"]["word"])
 
 
 
