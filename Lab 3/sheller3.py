ad54d0fbd713235590ac7994a75c94fbb33848fb
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-11 17:30:02 -0400

    Add files via upload

--- Lab 3/sheller3.py
+++ Lab 3/sheller3.py
@@ -6,11 +6,11 @@ from time import sleep
 
 #while True:
 subprocess.run(['arecord', '-D', 'hw:3,0', '-f', 'cd', '-c1', '-r', '48000', '-d', '5', '-t', 'wav', 'recorded_mono.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-result = subprocess.run(['python3', 'test_words3.py', 'recorded_mono.wav'], capture_output=True, text=True)
+result = subprocess.run(['python3', 'test_words3.py', 'recorded_mono.wav'], capture_output=True, text=True,stdout=PIPE)
     #sleep()
 
 print("result =", result)
 print("type result ", type(result))
 print("sub ", subprocess.CompletedProcess)
 
-result.stdout()
+print(" stdout = ", result.stdout.decode())
