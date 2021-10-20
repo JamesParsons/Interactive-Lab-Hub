8f1a1b1c4f6a3fd147c8f5f083d56493d0bc20f9
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-20 09:30:33 -0400

    Add files via upload

--- Lab 4/distance_joystick.py
+++ Lab 4/distance_joystick.py
@@ -23,7 +23,9 @@ def runExample():
 
     while True:
         
-        ToF.start_ranging()						 # Write configuration bytes to initiate measurement
+        ToF.start_ranging()
+	myJoystick.begin()
+	# Write configuration bytes to initiate measurement
         #time.sleep(.1)
         distance = ToF.get_distance()	 # Get the result of the measurement from the sensor
         #time.sleep(.1)
@@ -48,21 +50,21 @@ ToF = qwiic.QwiicVL53L1X()
 if (ToF.sensor_init() == None):					 # Begin returns 0 on a good init
     print("Sensor online!\n")
     
-while True:
-    try:
-        ToF.start_ranging()						 # Write configuration bytes to initiate measurement
-        time.sleep(.1)
-        distance = ToF.get_distance()	 # Get the result of the measurement from the sensor
-        time.sleep(.1)
-        ToF.stop_ranging()
+#while True:
+    #try:
+        #ToF.start_ranging()						 # Write configuration bytes to initiate measurement
+        #time.sleep(.1)
+        #distance = ToF.get_distance()	 # Get the result of the measurement from the sensor
+        #time.sleep(.1)
+        #ToF.stop_ranging()
 
-        distanceInches = distance / 25.4
-        distanceFeet = distanceInches / 12.0
+        #distanceInches = distance / 25.4
+        #distanceFeet = distanceInches / 12.0
 
-        print("Distance(mm): %s Distance(ft): %s" % (distance, distanceFeet))
+        #print("Distance(mm): %s Distance(ft): %s" % (distance, distanceFeet))
 
-    except Exception as e:
-        print(e)
+    #except Exception as e:
+        #print(e)
 
 
 if __name__ == '__main__':
