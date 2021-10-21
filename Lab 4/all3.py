43bd3e127eca5028941b089186baae634fd5c5ff
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-21 14:05:54 -0400

    Add files via upload

--- Lab 4/all3.py
+++ Lab 4/all3.py
@@ -4,6 +4,8 @@
 import board
 import busio
 import adafruit_ssd1306
+from PIL import Image, ImageDraw, ImageFont
+
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -12,15 +14,20 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
 oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-
+disp = oled
 
 # start with a blank screen
 oled.fill(0)
 # we just blanked the framebuffer. to push the framebuffer onto the display, we call show()
 oled.show()
 
-print("hello")
-oled.text("Arriving")
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+draw = ImageDraw.Draw(image)
+
+draw.text((10,10), "Arriving", font="Arial", fill=255)
+draw.show()
 
 #while True:
     
