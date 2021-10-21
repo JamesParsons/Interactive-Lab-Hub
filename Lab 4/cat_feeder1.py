9d7d8f68fdd36d8f82af945feee7ac583e9707e8
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-21 18:32:31 -0400

    Add files via upload

--- Lab 4/cat_feeder1.py
+++ Lab 4/cat_feeder1.py
@@ -4,6 +4,24 @@ import qwiic_joystick
 import time
 import sys
 import qwiic
+from adafruit_servokit import ServoKit
+import board
+import busio
+import adafruit_ssd1306
+
+# Set channels to the number of servo channels on your kit.
+# There are 16 channels on the PCA9685 chip.
+kit = ServoKit(channels=16)
+
+# Name and set up the servo according to the channel you are using.
+servo = kit.servo[2]
+
+# Set the pulse width range of your servo for PWM control of rotating 0-180 degree (min_pulse, max_pulse)
+# Each servo might be different, you can normally find this information in the servo datasheet
+servo.set_pulse_width_range(500, 2500)
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
 
 ################ fun with distance ###############################
 
@@ -11,6 +29,13 @@ def distance_fun(distance):
     
     if distance >= 0 and distance <= .3:
         print("within 4 inches")
+        
+        # Set the servo to 45 degree position
+        servo.angle = 45
+        time.sleep(2)
+        
+        servo.angle = 0
+        time.sleep(2)        
         
 ################# come or go ####################################
         
