742e82de20234b816deffc5a4ddee31e43164349
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-21 20:52:30 -0400

    Add files via upload

---
+++ Lab 4/oled1.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import board
+import busio
+import adafruit_ssd1306
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+
+
+#display.setTextSize(1)                 
+#display.setTextColor(WHITE)           
+#display.setCursor(0,0);                 
+#display.println("Hello, world!");   
+
+oled.setTextSize(5)
+oled.setTextColor("White")
+oled.setCursor(0,0)
+oled.println("Hello oled")
+
+#display.setTextColor(BLACK, WHITE);     
+#display.println("Hello, world!");        
+#display.setFont(&FreeMono9pt7b)
+#display.setTextColor(WHITE);
+#display.println("Hello, world!");
+#display.setFont(); 
+
+oled.show()
