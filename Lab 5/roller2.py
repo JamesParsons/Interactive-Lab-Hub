c81dc9602a3b628ea345379bd4b0cc5d447d5719
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-10-31 16:50:17 -0400

    Add files via upload

--- Lab 5/roller2.py
+++ Lab 5/roller2.py
@@ -118,7 +118,8 @@ def contours():
     
             ret, frame = cap.read()
             
-            cv2.imshow('frame',frame)
+            #cv2.imshow('frame',frame)
+            cv2.imshow('contours( press q to quit.)',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break    
             
