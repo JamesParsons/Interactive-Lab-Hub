e2f7366a58c4343d81fb271b72c6cc1785a71fde
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-11-01 18:42:28 -0400

    Add files via upload

--- Lab 5/roller2.py
+++ Lab 5/roller2.py
@@ -26,8 +26,8 @@ servo3 = kit.servo[3]
 # Set the pulse width range of your servo for PWM control of rotating 0-180 degree (min_pulse, max_pulse)
 # Each servo might be different, you can normally find this information in the servo datasheet
 servo.set_pulse_width_range(500, 2500)
-servo2.set_pulse_width_range(0,0)
-servo3.set_pulse_width_range(0,0)
+servo2.set_pulse_width_range(0,500)
+servo3.set_pulse_width_range(0,500)
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
