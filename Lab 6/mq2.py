e3642a7f6fa9337f43d22df65fb3ea1a00c5ce89
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-11-10 17:38:35 -0500

    Add files via upload

---
+++ Lab 6/mq2.py
@@ -0,0 +1,83 @@
+
+from __future__ import print_function
+import qwiic_joystick
+import time
+import sys
+import time
+import board
+import busio
+import adafruit_mpr121
+
+import paho.mqtt.client as mqtt
+import uuid
+
+
+
+
+
+topic = 'IDD/colors/JSP'
+
+def on_connect(client, userdata, flags, rc):
+    print(f"connected with result code {rc}")
+    client.subscribe(topic)
+
+def on_message(cleint, userdata, msg):
+    # if a message is recieved on the colors topic, parse it and set the color
+    if msg.topic == topic:
+        print("You pressed the button, fantastic")
+
+client = mqtt.Client(str(uuid.uuid1()))
+client.tls_set()
+client.username_pw_set('idd', 'device@theFarm')
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+client.loop_start()
+
+# this lets us exit gracefully (close the connection to the broker)
+def handler(signum, frame):
+    print('exit gracefully')
+    client.loop_stop()
+    exit (0)
+
+# when sigint happens, do the handler callback function
+signal.signal(signal.SIGINT, handler)
+
+myJoystick = qwiic_joystick.QwiicJoystick()
+myJoystick.begin()
+
+# our main loop
+while True:
+    
+    strval = ("X: %d, Y: %d, Button: %d" % ( \
+                myJoystick.horizontal, \
+                                    myJoystick.vertical, \
+                                    myJoystick.button))
+    
+    
+
+    if myJoystick.buttone == 0:
+        client.publish(topic, "Button pressed!")
+    
+    time.sleep(.01)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
