1ac0cb413851d27f930af05a98edfecaec383183
JamesParsons <89811189+JamesParsons@users.noreply.github.com>
2021-11-28 18:03:41 -0500

    Add files via upload

--- Lab 6/roller5.py
+++ Lab 6/roller5.py
@@ -147,12 +147,12 @@ def press(key):
         servo2.angle = 45 + two_adjuster
         servo3.angle = 45 + three_adjuster
         servo5.angle = 135 + five_adjuster
-        servo6.angle = 45 + six_adjuster                
+        servo6.angle = 135 + six_adjuster                
     if key == 'down':
         servo2.angle = 135 + two_adjuster
         servo3.angle = 135 + three_adjuster
         servo5.angle = 45 + five_adjuster
-        servo6.angle = 135 + six_adjuster        
+        servo6.angle = 45 + six_adjuster        
     if key == 'right':
         servo2.angle = 135 + two_adjuster
         servo3.angle = 135 + three_adjuster
